da94df038f359afa7670e4da6947dd682eedb79f
CoderAàyush <78433811+AayushkumarPathak@users.noreply.github.com>
2022-10-01 10:42:31 +0530

    Add files via upload

---
+++ INT_108_L3.py
@@ -0,0 +1,68 @@
+#casting
+#to specify the datatype of a variable use casting
+
+x = str(10)
+y = int(10)
+z = float(10)
+print(z+y)
+print(type(z+y))  #Combination gives <class:float>
+
+# --> short name like(x,y,z,...a)
+# --> description name like(name,age...etc)
+# --> variable must start with alphabet or _.
+# --> it will not start with numeric value.
+# --> only contain alpha-numeric(A-Z,0-9 and _ )
+
+myname='john'
+print(myname)
+
+my_name='kotlin'
+print(my_name)
+
+_myname='Ajay'
+print(_myname)
+
+myname1='Nitish'
+print(myname1)
+
+myname='Peter'
+print(myname)
+
+myName='Singh'
+print(myName)
+
+print(myname==myName)
+
+#___ERRoR__
+#   my name='Harsh_Jangra'
+#   print(my name)
+
+#camel case
+MyFirstName='Mukul'
+print(MyFirstName)
+
+#snake case
+My_First_Name='Madhav'
+print(My_First_Name)
+
+#assign multiple value
+x,y,z=1,2,3
+print(x)
+print(y)
+print(z)
+
+#output variables
+
+x='My Name is Ak'
+print(x)
+
+x='My'
+y='name'
+z='is Ak'
+print(x+' ' +y+' '+ z)
+
+p='hello'
+q='10'
+print(p+q)
+
+
