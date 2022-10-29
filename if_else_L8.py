7ec6910b122c9c9298629c502ff88d747a05bce8
AayushPathak <78433811+AayushkumarPathak@users.noreply.github.com>
2022-10-29 10:37:01 +0530

    Add files via upload

---
+++ if_else_L8.py
@@ -0,0 +1,44 @@
+""" a=40
+b=60
+c=50
+if(a<b):
+    print("True")
+else:
+    print("Flase") """
+
+#2nd Example
+""" x=40
+y=60
+z=80
+if(x<y):
+    print("True")
+elif(x==y):
+    print("both numbers are same")
+else:
+    print("False") """
+
+""" x=10
+y=20
+z=100 """
+##if(x<z and y<z):
+    ##print("Both the conditions are true)
+""" if(x>z or y<z):
+    print("true --> atleast one condition is met") """
+## Nested if else condition
+
+""" x=55
+if(x<60):
+    print("x is smaller than 60")
+    if(x<50):
+        print("x is smaller than 50")
+    else:
+       print("x is either equal or grater than 50 to less than 60")
+else:
+    print("x is either equal or greater than 60")
+ """
+
+ x=20
+ if x<40:
+
+else:
+    print("False")
---
+++ operators_L7.py
@@ -0,0 +1,19 @@
+#operators precidence
+""" print(10-4*2)
+
+print( 2**3**1) """
+
+##
+""" x=20
+y=30
+if x<y:
+    print("y is greater than x")
+else:
+    print("False") """
+
+collegeTime=10
+personenteringcollege=10
+if personenteringcollege<collegeTime:
+    print("Go to college")
+else:
+    print("")
